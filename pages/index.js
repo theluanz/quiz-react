7a52c83f4456c4fccfd128010e80157b58cd905c
theluanz <luanzuffo.lz@gmail.com>
2021-01-26 22:53:06 -0300

    aula 2 - input e  react Hooks

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React from 'react';
 import db from '../db.json';
-import Widget from '../src/components/Widget'
-// import QuizLogo from '../src/components/QuizLogo'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 // const BackgroundImage= styled.div`
 //   background-image: url(${db.bg});
@@ -24,17 +27,41 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-  return(
- 
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
+  return (
     <QuizBackground backgroundImage={db.bg}>
-      
+      <Head>
+        <title>Linux Quiz - Alura</title>
+        <link rel="icon" href={db.logo} type="image/x-icon" />
+        <meta property="og:image" content={db.bg} />
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
-           <h1>O quiz</h1>
-           </Widget.Header>
+            <h1>O quiz</h1>
+          </Widget.Header>
           <Widget.Content>
-            <p>zaaaaaaaazzzz zaaaaaaaazzzz</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infoEvento) {
+                  setName(infoEvento.target.value);
+                }}
+                placeholder="root name"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {' '}
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
@@ -44,10 +71,9 @@ export default function Home() {
             <p>zaaaaaaaazzzz zaaaaaaaazzzz</p>
           </Widget.Content>
         </Widget>
-        <Footer/>
-        <GitHubCorner projectUrl="https://github.com/theluanz"/>
+        <Footer />
+        <GitHubCorner projectUrl="https://github.com/theluanz" />
       </QuizContainer>
     </QuizBackground>
-
   );
 }
---
+++ pages/quiz.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function quizPage() {
+  return (
+    <div>
+      <h1>Tests</h1>
+    </div>
+  );
+}
